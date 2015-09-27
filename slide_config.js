b6f77793789b6c7cc5e6e7de946280adb3bf1bb6
André Tietz <andre.tietz@gmail.com>
2015-09-27 10:48:26 +0200

    presentation for the gdg hamburg

---
+++ slide_config.js
@@ -0,0 +1,38 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Dagger 2',
+    subtitle: 'Dependency Injection on Android',
+//    eventInfo: {
+//      title: 'Dependency Injection with Dagger 2',
+//      date: '10/14/2015'
+//    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/gdg-hamburg-android/gdg-hamburg-small.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    theme: ['gdghamburg'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Andre Tietz',
+    company: 'Android Developer @ mytaxi',
+    twitter: '@antietz',
+    github: 'https://github.com/Unic8/dagger2-introduction'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
